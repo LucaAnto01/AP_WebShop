53132d433d2e1e23b45241f46bb2a1c361e0583a
LucaAnto01 <luca.antognarelli@gmail.com>
2021-11-08 17:19:03 +0100

    Aggiornamento file creazione DB

--- db/creaDb_A&P.sql
+++ db/creaDb_A&P.sql
@@ -5,18 +5,17 @@ CREATE DATABASE  'A&P';
 USE 'A&P';
 
 CREATE TABLE fornitori(
-    id varchar(10) primary key NOT NULL AUTO_INCREMENT,
+    pIva varchar(11) primary key NOT NULL,
     email varchar(80) NOT NULL UNIQUE,
     pswd varchar(256) NOT NULL,
     ragioneSociale varchar(100) NOT NULL,
-    pIva varchar(11) NOT NULL,
     sede varchar(100),
     sitoWeb varchar(100),
 );
 
 CREATE TABLE prodotti(
-    codice varchar(10) primary key NOT NULL AUTO_INCREMENT,
-    fkIdFornitore varchar(10),
+    codice integer(10) primary key NOT NULL AUTO_INCREMENT,
+    fkPIva varchar(11) NOT NULL,
     nome varchar(100) NOT NULL,
     descrizione varchar(250),
     FOREIGN KEY (fkIdFornitore) REFERENCES AccountCategories(code)
@@ -27,4 +26,13 @@ CREATE TABLE utenti(
     email varchar(80) NOT NULL UNIQUE,
     pswd varchar(256) NOT NULL,
     residenza varchar(100), NOT NULL
+);
+
+CREATE TABLE fattura(
+    id integer(10) primary key NOT NULL AUTO_INCREMENT,
+    fkIntestatario varchar(50) NOT NULL,
+    fkCodiceProdotto integer(10) NOT NULL,
+    emissione date NOT NULL,
+    FOREIGN KEY (fkIntestatario) REFERENCES utenti(username),
+    FOREIGN KEY (fkCodiceProdotto) REFERENCES prodotti(codice)
 );
