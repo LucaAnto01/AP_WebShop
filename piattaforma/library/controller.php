247084388c03b58f96a7aeb8ff74a165924836b4
LucaAnto01 <luca.antognarelli@gmail.com>
2021-11-10 15:47:59 +0100

    Creazione file di funzione

--- piattaforma/library/controller.php
+++ piattaforma/library/controller.php
@@ -1,5 +1,16 @@
 <?php 
 
-    /**Gestione delle funzioni */
+    include "function/login.php";
+    include "function/logout.php";
+    include "function/registrazione.php";
+
+    /**Visualizzo l'azione richiesta dall'utente */
+    $azioneRichiesta = $_REQUEST['azione'];
+
+    /**Gestisco le richieste dell'utente */
+    switch($azioneRichiesta)
+    {   
+        default: echo("error_0");
+    }
 
 ?>
