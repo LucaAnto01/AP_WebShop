bb1473e6e50c70af1b9b9fcaf6baa07f9d369c08
LucaAnto01 <luca.antognarelli@gmail.com>
2021-11-10 15:22:40 +0100

    Creazione utenti per test futuri

--- db/creaUtentiDB_A&P.sql
+++ db/creaUtentiDB_A&P.sql
@@ -1,4 +1,11 @@
 /*Seleziono il database al fine di potervi operare*/
 USE 'A&P';
 
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'adminAdmin';
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin_admin';
+
+create user 'mariorossi' IDENTIFIED by 'cliente_mariorossi';
+
+create user 'ferrari' IDENTIFIED by 'fornitore_ferrari';
+
+/*SET PASSWORD FOR 'admin'@'localhost' = PASSWORD('LaPassword!'); per settare la password*/
+/*FLUSH PRIVILEGES; per aggiornare i privilegi*/
