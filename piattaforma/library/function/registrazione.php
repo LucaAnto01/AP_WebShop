9038f8f41aa669dae908846cb87918497f443c34
LucaAnto01 <luca.antognarelli@gmail.com>
2021-11-11 10:06:37 +0100

    Modifica bozza di funzioni

--- piattaforma/library/function/registrazione.php
+++ piattaforma/library/function/registrazione.php
@@ -2,6 +2,14 @@
     //TODO: quando farai la registrazione controlla che la mail non sia già presente NE nei fornitori NE nei clienti
     $connect = new mysqli($GLOBALS['host'], $GLOBALS['user'], $GLOBALS['password'], $GLOBALS['db']);
 
+    /**Funzione adibita alla registrazione di un cliente e quindi al rispettivo inserimento nel DB */
+    function registrazioneCliente()
+    {}
+
+    /**Funzione adibita alla registrazione di un fornitore e quindi al rispettivo inserimento nel DB */
+    function registrazioneFornitore()
+    {}
+
     /**Funzione adibita all'esecuzione della registrazione. A seconda del tipo vengono seguite procedure differenti
      * @param string tipo di registrazione
      */
