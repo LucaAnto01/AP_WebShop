26c4b24af9b81f87b72c342fb3e6d21e2505694e
LucaAnto01 <luca.antognarelli@gmail.com>
2021-11-11 10:21:14 +0100

    Modifica bozza funzione registrazione

--- piattaforma/library/function/registrazione.php
+++ piattaforma/library/function/registrazione.php
@@ -4,17 +4,29 @@
 
     /**Funzione adibita alla registrazione di un cliente e quindi al rispettivo inserimento nel DB */
     function registrazioneCliente()
-    {}
+    {
+        
+    }
 
     /**Funzione adibita alla registrazione di un fornitore e quindi al rispettivo inserimento nel DB */
     function registrazioneFornitore()
-    {}
+    {
+
+    }
 
     /**Funzione adibita all'esecuzione della registrazione. A seconda del tipo vengono seguite procedure differenti
      * @param string tipo di registrazione
      */
     function registrazione($tipoRegistrazione)
     {
+        if($tipoRegistrazione == "cliente")
+            registrazioneCliente();
+
+        else if($tipoRegistrazione == "fornitore")
+            registrazioneFornitore();
+
+        else
+            echo("error_1");
     }
 
 ?>
