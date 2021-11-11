f24aaae57b145d8ba26819bf8e2988423582ff87
LucaAnto01 <luca.antognarelli@gmail.com>
2021-11-11 09:47:57 +0100

    Completamento funzione login

--- piattaforma/library/function/login.php
+++ piattaforma/library/function/login.php
@@ -1,18 +1,17 @@
 <?php 
-    /*TODO:setta una variabile a o "cliente" o "fornitore" e mettila anche in sessione in base al login*/
 
     $connect = new mysqli($GLOBALS['host'], $GLOBALS['user'], $GLOBALS['password'], $GLOBALS['db']);
-    
-    /**Funzione che cerca la corrispondenza tra email e password all'interno del DB dedicato ai clienti
+
+    /**Funzione che cerca la corrispondenza tra email e password all'interno del DB nelle tabelle clienti | fornitori
      * @return int [1 -> trovato | -1 -> non trovato]
      */
-    function loginClienti($email, $passwordIn)
+    function checkLogin($emailIn, $passwordIn, $tabellaCheck)
     {
         //Protezione da injection
         $query = $GLOBALS['connect']->prepare("
                 SELECT *
-                FROM clienti
-                WHERE email = '".$email."' AND pswd = ?
+                FROM ".$tabellaCheck."
+                WHERE email = '".$emailIn."' AND pswd = ?
             ");
 
         $password = mysqli_real_escape_string($GLOBALS['connect'], $passwordIn);
@@ -20,42 +19,17 @@
         $query->execute();
         $result = $query->get_result();
 
-        if ($result->num_rows > 0) 
+        if ($result->num_rows > 0)
+        {
+            $_SESSION['email'] = $emailIn;
             return 1; //Corrispondenza trovata
+        } 
 
         return -1; //Nessuna corrispondenza trovata
     }
 
-    /**Funzione che cerca la corrispondenza tra email e password all'interno del DB dedicato ai fornitori
-     * @return int [1 -> trovato | -1 -> non trovato]
-     */
-    function loginFornitori($email, $passwordIn)
-    {
-        //Protezione da injection
-        $query = $GLOBALS['connect']->prepare("
-                SELECT *
-                FROM fornitori
-                WHERE email = '".$email."' AND pswd = ?
-            ");
-
-        $password = mysqli_real_escape_string($GLOBALS['connect'], $passwordIn);
-        $query->bind_param('s', $password);
-        $query->execute();
-        $result = $query->get_result();
-
-        if ($result->num_rows > 0) 
-            return 1; //Corrispondenza trovata
-
-        return -1; //Nessuna corrispondenza trovata
-    }
-
-    function login()
-    {
-
-        //Valori inseriti dall'utente
-        $emailReq = $_REQUEST["email"];
-        $passwordReq = $_REQUEST["password"];
-
+    function login($emailReq, $passwordReq)
+    {     
         if ((isset($_SESSION['email'])) && ($_SESSION['email'] == $emailReq))
         {
             echo "Sei già loggato, verrai rendirizzato alla pagina del tuo profilo tra 5 secondi...";
@@ -64,20 +38,21 @@
         }
         
         /**TODO: testa se funziona e guarda all'inizio cosa fare */
-        if((loginClienti($emailReq, $passwordReq) > 0) || (loginFornitori($emailReq, $passwordReq) > 0))
+        if(checkLogin($emailReq, $passwordReq, "clienti") > 0) //Controllo se l'utente che ha effettuato il login è un cliente
         {
-            $_SESSION['email'] = $emailReq;
-            echo "Trovato!";
+            $_SESSION['tipoAccount'] = "cliente";
+            echo "Trovato!"; //REVIEW: per test, poi rimuovilo
+        }
+
+        else if (checkLogin($emailReq, $passwordReq, "fornitori") > 0) //Controllo se l'utente che ha effettuato il login è un fornitore
+        {
+            $_SESSION['tipoAccount'] = "fornitore";
+            echo "Trovato!"; //REVIEW: per test, poi rimuovilo
         }
         
-        else
-            echo "Non trovato!";
-
-        /*else 
-        {
-            //REVIEW: facciamo altro o diamo un messaggio d'errore??? 
-            header('Location: Registrazione.html');
-        }*/        
+        else //REVIEW: facciamo altro o diamo un messaggio d'errore??? 
+            echo "Non trovato!"; //TODO: poi cambialo con un header verso la home page  
 
     }
+
 ?>
