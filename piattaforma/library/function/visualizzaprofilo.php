fc7cbcc4b1577d2fb9d95b2ea30745e841519a10
LucaAnto01 <luca.antognarelli@gmail.com>
2021-11-30 20:09:33 +0100

    Creazione funzione visualizzaProfilo()

---
+++ piattaforma/library/function/visualizzaprofilo.php
@@ -0,0 +1,14 @@
+<?php
+
+    $connect = new mysqli($GLOBALS['host'], $GLOBALS['user'], $GLOBALS['password'], $GLOBALS['db']);
+
+    function visualizzaProfilo()
+    {
+        //Nel caso in cui l'utente non sia loggato, o la sessione è scaduta, lo faccio riloggare
+        if(!isset($_SESSION['email']))
+            header("refresh:5; url=../../pages/registrazione.html");
+
+        
+    }
+
+?>
