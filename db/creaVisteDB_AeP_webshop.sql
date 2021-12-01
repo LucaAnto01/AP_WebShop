c3e6ff22fc992eeab9892c4cffeed34333be3101
LucaAnto01 <luca.antognarelli@gmail.com>
2021-12-01 16:50:22 +0100

    Aggiornamento vista vetrina_prodotti

--- db/creaVisteDB_AeP_webshop.sql
+++ db/creaVisteDB_AeP_webshop.sql
@@ -1,5 +1,5 @@
 /*Vista che forisce la lista dei prodotti disponibili associati ai rispettivi venditori*/
-CREATE VIEW vetrina_prodotti AS (SELECT p.nome AS prodotto, p.descrizione AS descrizione, p.costo AS costo, p.quantita AS quantita, f.ragioneSociale AS fornitore, p.id AS id_prodotto, f.pIva AS piva_fornitore
+CREATE VIEW vetrina_prodotti AS (SELECT p.nome AS prodotto, p.descrizione AS descrizione, p.costo AS costo, p.quantita AS quantita, f.ragioneSociale AS fornitore, p.id AS id_prodotto
                                     FROM fornitori f, prodotti p
                                     WHERE f.pIva = p.fkPIvaFornitore AND p.quantita > 0
                                     ORDER BY p.nome);
